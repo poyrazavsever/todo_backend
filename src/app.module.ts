7f0e693642cc59832436eae1cd94868c79814013
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-25 14:53:16 +0300

    feat: added mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosController } from './todos/todos.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/todo')],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController, TodosController],
   providers: [AppService],
   exports: [AppService],
