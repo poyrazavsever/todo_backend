6776dd7499b606337866055723a652ccdf2c255e
poyrazavsever <poyrazavsever@hotmail.com>
2025-06-26 14:00:01 +0300

    feat: updated todo controller

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -9,39 +9,39 @@ import {
   Patch,
 } from '@nestjs/common';
 import { TodosService } from './todos.service';
-import { User } from './schemas/todo.schema';
+import { Todo } from './schemas/todo.schema';
 
 @Controller('todo')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
   @Get()
-  findAll(): Promise<User[]> {
+  findAll(): Promise<Todo[]> {
     return this.todosService.findAll();
   }
 
   @Post()
-  create(@Body() todo: any): Promise<User> {
+  create(@Body() todo: any): Promise<Todo> {
     return this.todosService.create(todo);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): Promise<User> {
+  findOne(@Param('id') id: string): Promise<Todo> {
     return this.todosService.findOne(id);
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() todo: any): Promise<User> {
+  update(@Param('id') id: string, @Body() todo: Partial<Todo>): Promise<Todo> {
     return this.todosService.update(id, todo);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string): Promise<User> {
+  remove(@Param('id') id: string): Promise<Todo> {
     return this.todosService.delete(id);
   }
 
-  @Patch(':id/toggle')
-  toggleComplete(@Param('id') id: string): Promise<User> {
+  @Patch(':id')
+  toggleComplete(@Param('id') id: string): Promise<Todo> {
     return this.todosService.toggleComplete(id);
   }
 }
